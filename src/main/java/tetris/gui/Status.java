dfaf6151d153dccaa4157bda49960bb85df2893b
Beat Luginbühl <lugi@lugipfupf.ch>
2015-12-08 15:36:30 +0100

    use states to control game flow

---
+++ src/main/java/tetris/gui/Status.java
@@ -0,0 +1,12 @@
+package tetris.gui;
+
+/**
+ * The enumeration Status represents the status of the Tetris game.
+ *
+ * @author Stephan Fischli
+ * @version 2.0
+ */
+public enum Status {
+
+	READY, RUNNING, PAUSED, OVER
+}
---
+++ src/main/java/tetris/gui/StatusHandler.java
@@ -0,0 +1,15 @@
+package tetris.gui;
+
+/**
+ * The interface StatusHandler defines the method to change the status of the Tetris game.
+ *
+ * @author Stephan Fischli
+ * @version 2.0
+ */
+public interface StatusHandler {
+
+	/**
+	 * Changes the status of the game.
+	 */
+	void changeStatus() throws Exception;
+}
